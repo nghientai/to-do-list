36ecd31c9a3a85ca54d239eef8979406cd0c1bf5
Tai Nguyen <nghientai@gmail.com>
2020-03-27 23:37:01 -0700

    run build

---
+++ build/precache-manifest.3273573091e364f3ed89bddf88de02cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40d8ab5c5b842e56d218f4c1556df7a",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "815f801dc03d6dcdba89",
+    "url": "/to-do-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "815f801dc03d6dcdba89",
+    "url": "/to-do-list/static/js/2.dc004c06.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/to-do-list/static/js/2.dc004c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d3e4716993ae9988fe",
+    "url": "/to-do-list/static/js/main.f1d85c3f.chunk.js"
+  },
+  {
+    "revision": "b33ea1fa2caca91076bf",
+    "url": "/to-do-list/static/js/runtime-main.bf3648d9.js"
+  }
+]);
